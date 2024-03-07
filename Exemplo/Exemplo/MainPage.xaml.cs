7223cf428578b711c906d77bc3fc1083bd944ea0
PaolaEtec <paola.condori@etec.sp.gov.br>
2024-03-07 12:02:50 -0300

    chore: Initial commit

---
+++ Exemplo/Exemplo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace Exemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Btn.Text = $"Clicked {count} time";
+            else
+                Btn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(Btn.Text);
+        }
+
+        private void OnGreetClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            GreetLabel.Text = $"Hello, {name}!";
+        }
+    }
+    // comentário
+
+}
